15264900cab440878a738498ec7089d16f206067
zheyanliu99 <zl3119@columbia.edu>
2021-09-28 22:43:58 -0400

    add method 3 and comment on method1

--- Bertrand_Paradox.py
+++ Bertrand_Paradox.py
@@ -1,3 +1,6 @@
+# For this assignment, I will only use method 2 and method 3.
+# Still trying to figure out why method 1 converge to 0.36
+
 # %% import packages
 import random
 import math
@@ -6,17 +9,17 @@ import math
 # function to generate y according to x
 def generate_y(x):
     r = random.random()
-    if r < 0.5:
+    if r <= 0.5:
         y = math.sqrt(1 - x**2)
     else:
         y = -math.sqrt(1 - x**2)
     return y
 
-def Bertrand(n):
+def Bertrand1(n):
     c = math.sqrt(3)
     cnt = 0
-    # randomly generate two Abscissas
     for _ in range(n):
+        # randomly generate two Abscissas
         x1 = 2 * random.random() - 1 
         x2 = 2 * random.random() - 1
         y1 = generate_y(x1) 
@@ -27,13 +30,12 @@ def Bertrand(n):
             cnt += 1
     return cnt/n
 
-
 # %% Second simulation
 def Bertrand2(n):
     c = math.sqrt(3)
     cnt = 0
-    # randomly generate two degrees
     for _ in range(n):
+        # randomly generate two degrees
         degree1 = 2*math.pi*random.random()
         degree2 = 2*math.pi*random.random()
         x1 = math.cos(degree1)
@@ -46,8 +48,20 @@ def Bertrand2(n):
             cnt += 1
     return cnt/n
 
+# %% Third simulation
+def Bertrand3(n):
+    cdegree = 120
+    cnt = 0
+    for _ in range(n):
+        # randomly generate one degree
+        degree = 360 * random.random()
+        # if the degree bigger than 120, it has longer chord
+        if cdegree < degree:
+            cnt += 1
+    return cnt/n
 # %% Simulation and compare
 n = 1000000
-delta = 0.02
-Bertrand(n) - Bertrand2(n) < delta # Not Converge to same number
-# The first one converged to 0.36 and the second one 0.33
+# print('Bertrand1', Bertrand1(n))
+print('Bertrand2', Bertrand2(n))
+print('Bertrand3', Bertrand3(n))
+
